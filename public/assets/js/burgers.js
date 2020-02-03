cbf0a715c3750c503a9317f2c2ce1726e31c5fd9
Mahir Erden <mahirerden@gmail.com>
2020-02-02 20:24:25 -0500

    files created

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,42 @@
+$(function() {
+
+  $(".changeDevour").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var devouredChanged = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredChanged
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#burger").val().trim(),
+      devoured: false
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
-const mysql = require('mysql')
+var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
 
 const app = express()
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080, () => console.log('All is OK!'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
+
+
